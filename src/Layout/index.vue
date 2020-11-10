46613bb3076f0578e04294abe2cd5f3cf175b82e
fangLi <747174405@qq.com>
2020-11-10 11:46:24 +0800

    init:客户端仓库初始化

---
+++ src/Layout/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="header">{{header}}</div>
+  <router-view />
+</template>
+
+<script>
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  setup() {
+    const header = ref('header')
+    return {
+      header
+    }
+  }
+})
+</script>
+
+<style lang="less" scoped>
+.header{
+  width: 100%;
+  height: 30px;
+  border-bottom: 1px solid #ccc;
+}
+</style>
---
+++ src/views/MainPage/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="main">
+    <div class="roleInfo">roleInfo
+      <div class="">equment</div>
+    </div>
+    <div class="">map
+      <div class="">options</div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  setup() {
+    return {
+
+    }
+  }
+})
+</script>
+
+<style lang="less" scoped>
+.main{
+  height: calc(100vh - 30px);
+  width: 1200px;
+  margin: 0 auto;
+  display: flex;
+  .roleInfo{
+    width: 150px;
+    height: 100%;
+  }
+}
+</style>
