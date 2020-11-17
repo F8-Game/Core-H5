b538a5386e51b714837076ddf2c8e113dc56c412
244406664@qq.com <diamondTao520>
2020-11-17 16:01:58 +0800

    加入ws登录验证失败逻辑

--- src/util/game.js
+++ src/util/game.js
@@ -1,6 +1,7 @@
 import WS from './ws.js'
-import { getToken } from './auth.js'
+import { getToken, removeToken } from './auth.js'
 import { sleep } from './tools'
+import router from '../router/index.js'
 
 const ws = new WS('ws://119.3.236.163:8081')
 
@@ -8,6 +9,11 @@ const game = {
   async login() {
     const result = await ws.send('login.userLogin', { username: '封andy为土地公', token: getToken() })
     console.log('async result: ', result)
+    // 验证失败处理
+    if (result.code === 500) {
+      removeToken()
+      router.push({ name: 'Login' })
+    }
   }
 }
 
